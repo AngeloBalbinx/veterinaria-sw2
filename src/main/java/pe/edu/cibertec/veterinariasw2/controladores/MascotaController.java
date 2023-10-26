a53d45f2963cfe2673dc4b223203665bb859239e
AngeloBalbinx <balbinangelo94@gmail.com>
2023-10-25 20:22:52 -0500

    wa

--- src/main/java/pe/edu/cibertec/veterinariasw2/controladores/MascotaController.java
+++ src/main/java/pe/edu/cibertec/veterinariasw2/controladores/MascotaController.java
@@ -1,31 +1,32 @@
 package pe.edu.cibertec.veterinariasw2.controladores;
 
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import pe.edu.cibertec.veterinariasw2.entidades.Mascota;
 import pe.edu.cibertec.veterinariasw2.repositorios.MascotaRepository;
 
 
 @RestController
+@AllArgsConstructor
 public class MascotaController {
 
     // veterinaria-don-tito.com
 
     MascotaRepository mascotaRepository; // si yo necesito para que funcione (tu dependes)
-    MascotaController(MascotaRepository mascotaRepository){
+    /*MascotaController(MascotaRepository mascotaRepository){
         this.mascotaRepository = mascotaRepository;
-            
-        
-    }
+    }*/
 
 
-    @RequestMapping("obtenerMascotas")
+    @GetMapping("obtenerMascotas")
     public List<Mascota> listarMascotas(){
         return mascotaRepository.findAll();
     }
